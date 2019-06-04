55645798ef78105f2859c264a50b79238383cbee
signdoubt <upgrade.outdated@gmail.com>
2019-06-05 01:41:05 +0900

    add license management

---
+++ src/components/Licenses.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row card">
+    Dependencies:
+    <table id="dependencies">
+      <tr>
+        <td>Name</td>
+        <td>License</td>
+      </tr>
+      <tr v-for="(license, key) in licenses">
+        <td><a :href="license.repository">{{ key }}</a></td>
+        <td><a :href="license.licenseFile">{{ license.licenses }}</a></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import licensesJson from './dependencies'
+export default {
+  data() {
+    return {
+      licenses: licensesJson
+    };
+  },
+}
+</script>
+<style>
+#dependencies table, #dependencies td {
+  border-collapse: collapse;
+  border:1px solid #888;
+}
+</style>
