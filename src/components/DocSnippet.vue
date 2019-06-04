da836ef0d0799913856f0669984bf228f1f881cc
signdoubt <upgrade.outdated@gmail.com>
2019-06-05 01:38:00 +0900

    add document and gallery

---
+++ src/components/DocSnippet.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div>
+      <a :href="url">{{ filename }}</a>:
+    </div>
+    <FurumaiApp1
+      v-if="version === 1"
+      v-bind:furumaiData="furumaiData"
+      v-bind:editorMode="false"
+    ></FurumaiApp1>
+    <div v-else-if="version === -1">version = -1</div>
+    <div v-else>version error: {{version}}</div>
+    <!--
+    <iframe
+      class="docSnippet"
+      :src="url"
+    ></iframe>
+    -->
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator";
+import FurumaiApp1 from "@/components/FurumaiApp1.vue";
+import {DataEncoderDecoderV1} from "@/io/DataEncoderDecoderV1";
+@Component({
+  components: {FurumaiApp1}
+})
+export default class Docs extends Vue {
+  private static codec = new DataEncoderDecoderV1()
+
+  private version: number = -1
+  private furumaiData: any = {}
+
+  @Prop({default: 'no name'}) filename!: string
+  @Prop() url!: string
+
+  public mounted() {
+    if (this.url) {
+      const tokens = this.url.split('/')
+      if (tokens[2] === Docs.codec.formatVersion) {
+        const data = Docs.codec.decode(tokens[3])
+        const {version, ...rest} = data
+        if (version && version > 0) {
+          this.version = version
+          this.furumaiData = rest
+        } else {
+          throw new Error(`app version is not specified`)
+        }
+      } else {
+        throw new Error(`unsupported data format version: ${tokens[2]}`)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/tools/tools.ts
@@ -0,0 +1,44 @@
+import {DataEncoderDecoderV1} from '../io/DataEncoderDecoderV1'
+import AppParams1 from '@/components/AppParams1'
+
+const fs = require('fs')
+const codec = new DataEncoderDecoderV1()
+
+function generateSnippet(dir: string, files: string[]) {
+  files.forEach((filename: any) => {
+    fs.readFile(`${dir}/${filename}`, 'utf8', (err: any, text: any) => {
+      if (err) {
+        throw err
+      }
+      const params1: AppParams1 = {
+        code: text,
+        animation: {
+          active: true,
+        },
+      }
+      const params = {
+        version: 1,
+        ...params1
+      }
+      const iframe = `<DocSnippet
+  filename="${dir}/${filename}"
+  :url="urlOf('${codec.formatVersion}/${codec.encode(params)}')"
+></DocSnippet>
+`
+      console.log(iframe)
+    })
+  })
+}
+
+function generate(dir: string) {
+  fs.readdir(dir, (err: any, files: any[]) => {
+    if (err) {
+      throw err
+    } else {
+      generateSnippet(dir, files.sort())
+    }
+  })
+}
+
+generate('./gallery/examples')
+generate('./gallery/docs')
