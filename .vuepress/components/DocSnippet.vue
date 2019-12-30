75130c8b67200914df4bd539f6df3bb980d83ae5
Ichiro Arai <upgrade.outdated@gmail.com>
2019-12-30 23:14:42 +0900

    doc vue package

--- .vuepress/components/DocSnippet.vue
+++ .vuepress/components/DocSnippet.vue
@@ -31,7 +31,7 @@
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-property-decorator'
 import FurumaiApp1 from '@/components/FurumaiApp1.vue'
-import {DataEncoderDecoderV1} from '@/io/DataEncoderDecoderV1'
+import {DataEncoderDecoderV1} from '@/utils/types'
 
 @Component({
   components: {FurumaiApp1},
